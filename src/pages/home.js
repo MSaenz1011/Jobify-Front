67428e3045003defed1a368ed9ccf6fd7fbe3edd
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-05-10 13:20:12 -0500

    first Next Project

---
+++ src/pages/home.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className='font-bold text-2xl lg:text-3xl text-black pb-8 mt-8 text-center'>
+        Looking for a job?
+      </h1>
+
+      <div className='flex justify-evenly mt-10 mb-10'>
+        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>
+              Jobify: Your best option
+            </div>
+            <p className='text-gray-700 text-base'>
+              With a desirable catalogue, Jobify is a nice option to check all
+              offers from the IT world. Give us a try, you will not be
+              disappointed
+            </p>
+          </div>
+        </div>
+
+        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>Check the jobs</div>
+            <p className='text-gray-700 text-base'>
+              You can check the jobs we currently have, see if one of them is
+              the one you are looking for
+            </p>
+
+            <Link
+              href='/jobs'
+              className='font-medium text-primary-600 underline underline-offset-1'
+            >
+              See all the Jobs
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
